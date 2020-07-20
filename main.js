7049e3a49c53c7411cd6011c8fd6beede8178828
Ankita <2706ankita@gmail.com>
2020-07-20 21:46:41 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("button-id").addEventListener("click", function() {
+    alert("Thank you for submitting...");
+});
+
+document.getElementById("readMore").addEventListener("click", function() {
+    alert("This is it! There is no more.");
+});
